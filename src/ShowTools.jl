6cc6ff46fb9652b181e5549db2c75ab39d706c79
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2020-07-28 21:05:13 -0400

    Some ideas

--- src/ShowTools.jl
+++ src/ShowTools.jl
@@ -32,4 +32,21 @@ function show_fields(io::IO, @nospecialize(x))
     end
 end
 
+"""
+When `show(f)` reveals `typeof(f)`, then it's redundant to include the type params.
+In those cases, use this representation.
+"""
+function show_default_no_typeparams(io::Base.IO, x)
+    print(io, typeof(x).name)
+    print(io, "(")
+    show_fields(io, x)
+    print(io, ")")
+end
+
+is_show_type_evident(x) = is_show_type_evident(typeof(x))
+is_show_type_evident(t::Type{<:Any}) = false
+is_show_type_evident(t::Type{<:Tuple}) = all(map(is_show_type_evident, t.parameters))
+is_show_type_evident(t::Type{<:Base.OneTo}) = true
+# I believe you want this to be true even though e.g. Int32 and Int64 print the same and so the type is not evident.
+is_show_type_evident(t::Type{Int64}) = true
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,52 @@
-using ShowTools
-using Test
+using SafeTestsets: @safetestset
 
-@testset "ShowTools.jl" begin
-    # Write your tests here.
+# r"Main\..*\."
+
+@safetestset "no type param" begin
+    io = IOBuffer()
+    struct Foo
+        x::Int64
+    end
+
+    show(io, Foo(3))
+    @show String(take!(io)) # because of safetestset it will be like "Main.##257.Foo(3)""
+end
+
+@safetestset "type param" begin
+    using ShowTools: ShowTools
+    io = IOBuffer()
+    struct Foo{T}
+        x::T
+    end
+
+    Base.show(io::Base.IO, x::Foo) = ShowTools.show_default_no_typeparams(io, x)
+    show(io, Foo(3))
+    @show String(take!(io)) # because of safetestset it will be like "Main.##123.Foo(3)""
+    #Foo(3)
+end
+
+@safetestset "type param" begin
+    using ShowTools: ShowTools
+    io = IOBuffer()
+    struct Foo{S, T}
+        x::T
+    end
+
+    Base.show(io::Base.IO, x::Foo) = ShowTools.show_default_no_typeparams(io, x)
+    show(io, Foo{Some{Float64}, Int64}(3))
+    @show String(take!(io))
+    # maybe Foo{Some{Float64}, _}(3)
+end
+
+@safetestset "type param" begin
+    using ShowTools: ShowTools
+    io = IOBuffer()
+    struct Foo{S, T}
+        x::T
+    end
+
+    # Base.show(io::Base.IO, x::Foo) = ShowTools.show_default_no_typeparams(io, x)
+    show(io, Foo{:bitsbits, Int64}(3))
+    @show String(take!(io))
+    # maybe Foo{:bitsbits, _}(3)
 end
